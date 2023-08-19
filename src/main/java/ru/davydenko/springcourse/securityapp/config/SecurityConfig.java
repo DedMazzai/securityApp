eddd4dd0675329b7c6e0145ac74b2fb3e88fa906
DedMazzai <ded_mazzai@inbox.ru>
2023-08-19 15:40:19 +0300

    Добавил фильты filterChain

--- src/main/java/ru/davydenko/springcourse/securityapp/config/SecurityConfig.java
+++ src/main/java/ru/davydenko/springcourse/securityapp/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.davydenko.springcourse.securityapp.services.MemberDetailService;
 
 @Configuration
@@ -26,7 +29,24 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->authorize
+                        .requestMatchers("/auth/login", "/error")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error"));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
