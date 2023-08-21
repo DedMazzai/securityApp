c7699dddb604d851e3e3ba6c29f52785a2e6f7e8
DedMazzai <ded_mazzai@inbox.ru>
2023-08-21 13:49:52 +0300

    Добавил авторизацию с помощью аннотации @PreAuthorize

--- src/main/java/ru/davydenko/springcourse/securityapp/config/SecurityConfig.java
+++ src/main/java/ru/davydenko/springcourse/securityapp/config/SecurityConfig.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.davydenko.springcourse.securityapp.services.MemberDetailService;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final MemberDetailService detailService;
@@ -34,8 +34,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin")
-                        .hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/error", "/auth/registration")
                         .permitAll()
                         .anyRequest().hasAnyRole("USER", "ADMIN"))
--- src/main/java/ru/davydenko/springcourse/securityapp/controllers/HelloController.java
+++ src/main/java/ru/davydenko/springcourse/securityapp/controllers/HelloController.java
@@ -1,19 +1,30 @@
 package ru.davydenko.springcourse.securityapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.davydenko.springcourse.securityapp.security.MemberDetails;
+import ru.davydenko.springcourse.securityapp.services.AdminService;
 
 @Controller
 public class HelloController {
 
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
-    public String sayHello(){return "hello";}
+    public String sayHello() {
+        return "hello";
+    }
 
     @GetMapping("/showUserInfo")
-    public String showUserInfo(){
+    public String showUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         var memberDetails = (MemberDetails) authentication.getPrincipal();
         System.out.println(memberDetails.getMember());
@@ -21,7 +32,8 @@ public class HelloController {
     }
 
     @GetMapping("/admin")
-    public String adminPage(){
+    public String adminPage() {
+        adminService.doAdminStaff();
         return "admin";
     }
 }
